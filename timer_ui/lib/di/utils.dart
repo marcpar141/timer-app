100da37d8832bb355ebe51243f1f2b6c711536be
psapekinfotower <psapek@infotower.pl>
2023-12-03 00:11:40 +0100

    updated server address

--- timer_ui/lib/di/utils.dart
+++ timer_ui/lib/di/utils.dart
@@ -2,5 +2,5 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:timer_ui/utils/server_connection.dart';
 
 final utils = [
-  Dependency((i) => ServerConnection("ws://192.168.1.108:5000/timer")),
+  Dependency((i) => ServerConnection("ws://server:5000/timer")),
 ];
