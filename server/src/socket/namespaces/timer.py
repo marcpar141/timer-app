af5ca316873ebd7b0514f6d35f668f7c194b6371
marcpar141 <marceliparandyk@gmail.com>
2023-12-02 19:47:13 +0100

    added fields to class init

--- server/src/socket/namespaces/timer.py
+++ server/src/socket/namespaces/timer.py
@@ -1,10 +1,14 @@
 from flask import request
+from flask_socketio import SocketIO, join_room
 from flask_socketio.namespace import Namespace
+from typing import Union
 
 
 class TimerNamespace(Namespace):
     def __init__(self):
         super().__init__("/timer")
+        self.socketio: SocketIO = self.socketio
+        self.timer_control_event_name = "control"
 
     def on_connect(self):
         print(f"{self.__get_sid()} connected!")
