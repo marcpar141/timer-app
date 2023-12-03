ace96bd2891711cda0e7fee081fb731083eec688
marcpar141 <marceliparandyk@gmail.com>
2023-12-03 02:16:42 +0100

    added handling state event in order to emit messages for state event client

--- server/src/socket/namespaces/timer.py
+++ server/src/socket/namespaces/timer.py
@@ -3,13 +3,14 @@ import json
 from flask import request
 from flask_socketio import join_room, leave_room
 from flask_socketio.namespace import Namespace
-from typing import Union
+from typing import Union, Dict
 
 
 class TimerNamespace(Namespace):
     def __init__(self):
         super().__init__("/timer")
         self.timer_control_event_name = "control"
+        self.timer_state_event_name = "state"
 
     def on_connect(self):
         if self.__is_system_client():
@@ -17,6 +18,14 @@ class TimerNamespace(Namespace):
         sid = self.__get_sid()
         join_room(sid)
         self.emit(self.timer_control_event_name, json.dumps({"command": "start", "roomName": sid}))
+
+    def on_state(self, state: Dict[str, Union[int, str]]):
+        state = json.loads(state)
+        room_name = self.__get_room_name_from_state(state)
+        if room_name is None:
+            return
+        time = self.__get_time_from_state(state)
+        self.emit(self.timer_state_event_name, json.dumps(time), room_name)
 
     def on_disconnect(self):
         if self.__is_system_client():
@@ -33,3 +42,10 @@ class TimerNamespace(Namespace):
 
     def __get_system_client_info(self) -> Union[str, None]:
         return request.headers.environ.get('HTTP_CLIENT')
+
+    def __get_time_from_state(self, state: Dict[str, Union[int, str]]) -> Union[Dict[str, int], None]:
+        time = state.get("time", 120)
+        return {"time": time} if time is not None else None
+
+    def __get_room_name_from_state(self, state: Dict[str, Union[int, str]]) -> Union[str, None]:
+        return state.get("roomName")
